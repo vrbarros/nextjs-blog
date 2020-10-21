41a080dc07052446e17b2b5073ba523fc75fd96c
vrbarros <victor.barros@windowslive.com>
2020-10-21 20:08:07 -0300

    New study project using Next.js

---
+++ pages/404.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+import Layout from '../components/layout';
+
+function Custom404() {
+  return (
+    <Layout>
+      <Head>
+        <title>404</title>
+      </Head>
+      <h1>404 - Page Not Found</h1>
+    </Layout>
+  );
+}
+
+export default Custom404;
